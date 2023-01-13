ffcf84492249cae9603953c23c3c1aa77361e843
Luiz Victor  Nascimento Andrade <lvictor@digiboard.com.br>
2023-01-13 07:42:00 -0400

    Fazendo validação de dados.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import { ItemsController } from './controllers/ItemsController';
 import { PointsController } from './controllers/PointsController';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -17,7 +18,22 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post('/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(), // Usar regex para validar a separação por vírgula.
+    })
+  }),
+  pointsController.create
+);
 routes.delete('/points/:id', pointsController.delete);
 
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,26 +3,23 @@ import express, { urlencoded } from 'express';
 import path from 'path';
 import routes from './routes';
 import '../src/database/conn';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors({
   origin: "*"
 }));
+
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(express.json());
 app.use(urlencoded({
   extended: true,
 }));
 
-const users = [
-  'Diego',
-  'Luiz',
-  'Robson',
-  'Daniel',
-  'Cleiton'
-]
 app.use('/', routes);
+
+app.use(errors());
 
 app.listen(3333, () => {
   console.log('🚀 Sever is running ! 🚀');
